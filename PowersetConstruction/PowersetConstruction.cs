0e03a4dcdd82339f06f2a2e7e926009519f18372
Mitja Bezenšek <mitja.bezensek@gmail.com>
2015-04-30 15:47:26 +0200

    Initial commit

---
+++ PowersetConstruction/PowersetConstruction.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace PowersetConstruction{
+   internal class PowersetConstruction{
+      public static DFSM Convert(NDFSM nd){
+         var Q = new List<string>();
+         var Sigma = nd.Sigma.ToList();
+         var Delta = new List<Transition>();
+         var Q0 = nd.Q0.ToList();
+         var F = new List<string>();
+
+         var processed = new List<string>();
+         var queue = new Queue<string>(Q0);
+
+         while (queue.Count > 0){
+            var setState = queue.Dequeue();
+            processed.Add(setState);
+            Q.Add(setState);
+
+            var statesInCurrentSetState = setState.Split(',').ToList();
+            foreach (var state in statesInCurrentSetState){
+               if (nd.F.Contains(state)){
+                  F.Add(setState);
+                  break;
+               }
+            }
+            var symbols = nd.Delta
+               .Where(t => statesInCurrentSetState.Contains(t.StartState))
+               .Select(t => t.Symbol)
+               .Distinct();
+            foreach (var symbol in symbols){
+               var reachableStates =
+                  nd.Delta
+                     .Where(t => t.Symbol == symbol &&
+                                 statesInCurrentSetState.Contains(t.StartState))
+                     .OrderBy(t => t.EndState).
+                     Select(t => t.EndState);
+               var reachableSetState = string.Join(",", reachableStates);
+
+               Delta.Add(new Transition(setState, symbol, reachableSetState));
+
+               if (!processed.Contains(reachableSetState)){
+                  queue.Enqueue(reachableSetState);
+               }
+            }
+         }
+         return new DFSM(Q, Sigma, Delta, Q0, F);
+      }
+   }
+}
---
+++ PowersetConstruction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using PowersetConstruction;
+
+namespace NFSM{
+   internal class Program{
+      private static void Main(){
+         var Q = new List<string>{"q0", "q1", "q2", "q3"};
+         var Sigma = new List<char>{'0', '1'};
+         var Delta = new List<Transition>{
+            new Transition("q0", '0', "q0"),
+            new Transition("q0", '1', "q0"),
+            new Transition("q0", '1', "q1"),
+            new Transition("q1", '0', "q2"),
+            new Transition("q1", '1', "q2"),
+            new Transition("q2", '0', "q3"),
+            new Transition("q2", '1', "q3"),
+         };
+         var Q0 = new List<string>{"q0"};
+         var F = new List<string>{"q3"};
+         var ndfsm = new NDFSM(Q, Sigma, Delta, Q0, F);
+         var dfsm = PowersetConstruction.PowersetConstruction.Convert(ndfsm);
+      }
+   }
+}
